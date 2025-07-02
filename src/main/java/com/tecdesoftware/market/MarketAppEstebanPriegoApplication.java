3156be368bbcf6e46cd1443d2c62b8f27f28a93e
JJJEEEz <juanestebanperezvelazquez@gmail.com>
2025-07-02 13:33:04 -0600

    feat: initial product API with controller, service, and repository

--- src/main/java/com/tecdesoftware/market/MarketAppEstebanPriegoApplication.java
+++ src/main/java/com/tecdesoftware/market/MarketAppEstebanPriegoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MarketAppJuanPerezApplication {
+public class MarketAppEstebanPriegoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MarketAppJuanPerezApplication.class, args);
+		SpringApplication.run(MarketAppEstebanPriegoApplication.class, args);
 	}
 
 }
--- src/main/java/com/tecdesoftware/market/persistence/ProductoRepository.java
+++ src/main/java/com/tecdesoftware/market/persistence/ProductoRepository.java
@@ -1,15 +1,67 @@
 package com.tecdesoftware.market.persistence;
+
+import com.tecdesoftware.market.domain.Product;
+import com.tecdesoftware.market.domain.repository.ProductRepository;
 import com.tecdesoftware.market.persistence.crud.ProductoCrudRepository;
 import com.tecdesoftware.market.persistence.entity.Producto;
+import com.tecdesoftware.market.persistence.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
+import java.util.Optional;
 
-public class ProductoRepository
-{
+//Esta anotación le dice a Spring que este archivo se enlaza con la BD
+@Repository
+public class ProductoRepository implements ProductRepository {
+
+    //Auto inyectado: Spring se encarga de crear la instancia
+    @Autowired
     private ProductoCrudRepository productoCrudRepository;
+    @Autowired
+    private ProductMapper productMapper;
 
-    //Equivalente a poner SELECT * FROM productos
-    public List<Producto> getAll() {
-        //Se castea de iterable a lista
-        return (List<Producto>) productoCrudRepository.findAll();
+    //Equivalente a poner Select * from productos
+    @Override
+    public List<Product> getAll() {
+        //Se castea de Iterable a Lista
+        List<Producto> productos = (List<Producto>) productoCrudRepository.findAll();
+        return productMapper.toProducts(productos);
     }
+
+    @Override
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        List<Producto> productos = productoCrudRepository.findByIdCategoriaOrderByNombreAsc(categoryId);
+        return Optional.of(productMapper.toProducts(productos));
+    }
+
+    @Override
+    public Optional<List<Product>> getScarceProducts(int quantity) {
+        Optional<List<Producto>> productos = productoCrudRepository.findByCantidadStockLessThanAndEstado(quantity, true);
+        //No hay un mapper que convierta una lista de opcionales, por lo que a los productos se le hará un map
+        return productos.map(prods -> productMapper.toProducts(prods));
+    }
+
+    //Obtener un producto dado el id
+    @Override
+    public Optional<Product> getProduct(int productId) {
+        return productoCrudRepository.findById(productId).map(producto -> productMapper.toProduct(producto));
+    }
+
+    //Guardar un producto
+    @Override
+    public Product save(Product product) {
+        Producto producto = productMapper.toProducto(product);
+        return productMapper.toProduct(productoCrudRepository.save(producto));
+    }
+
+
+    //Eliminar producto por IdProducto
+    @Override
+    public void delete(int productId) {
+        productoCrudRepository.deleteById(productId);
+    }
+
+
+
 }
